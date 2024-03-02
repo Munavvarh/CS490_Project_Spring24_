f0a2c17fa428d578a9c8fab2776971678444f228
Munavvarh <munavvarh123@gmail.com>
2024-03-02 12:56:58 -0500

    Implemented NavBar and Updated homepage/footer styling

---
+++ web/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+// web/src/components/Footer/Footer.js
+
+const Footer = () => {
+  return (
+    <footer className="global-footer">
+      <p className="global-footer-text">© 2024 SyntaxSwitch. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
