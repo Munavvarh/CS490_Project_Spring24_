838802cccef4ad90f16c2a66dec3850e4bcb7226
Navodith Shankar <99934649+navos2003@users.noreply.github.com>
2024-03-04 14:54:10 -0500

    Adding Translation Page

--- web/src/Routes.jsx
+++ web/src/Routes.jsx
@@ -12,6 +12,7 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/translation-output" page={TranslationOutputPage} name="translationOutput" />
       <Route path="/home" page={HomePage} name="home" />
       <Route notfound page={NotFoundPage} />
     </Router>
