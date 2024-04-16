a410fbc284dffee5fc62c9056f5e1e7bfa0ddafc
Navodith Shankar <99934649+navos2003@users.noreply.github.com>
2024-04-16 16:46:40 -0400

    Update TranslationHistoriesCell.jsx

--- web/src/components/TranslationHistoriesCell/TranslationHistoriesCell.jsx
+++ web/src/components/TranslationHistoriesCell/TranslationHistoriesCell.jsx
@@ -1,6 +1,3 @@
-// Import necessary dependencies from RedwoodJS
-import React, { useState } from 'react'
-
 import { Link, routes } from '@redwoodjs/router'
 
 //import { MetaTags } from '@redwoodjs/web'
