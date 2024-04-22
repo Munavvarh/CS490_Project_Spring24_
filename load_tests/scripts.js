003999d6f048b9942802379a6435e5de292b2601
printWendell <superwendell46@gmail.com>
2024-04-22 14:42:08 -0400

    add more pages and use the deployed vercel link instead of the localhost

--- load_tests/scripts.js
+++ load_tests/scripts.js
@@ -1,9 +1,12 @@
 import http from 'k6/http'
-import { sleep } from 'k6'
+import { sleep, check } from 'k6'
 
 export const options = {
-  vus: 10,
-  duration: '30s',
+  stages: [
+    { duration: '30s', target: 25 }, //ramp up
+    { duration: '1m', target: 50 }, // stable
+    { duration: '20s', target: 20 }, // ramp-down to 0 users
+  ],
   ext: {
     loadimpact: {
       // Project: CS490 Project
@@ -15,6 +18,21 @@ export const options = {
 }
 
 export default function () {
-  http.get('https://test.k6.io')
-  sleep(1)
+  const pages = [
+    '/home',
+    '/documentation',
+    '/contact-us',
+    '/translation-output',
+    '/login',
+  ]
+  // http.get('http://localhost:8910/home')
+  for (const page of pages) {
+    const res = http.get('https://cs-490-project-spring24.vercel.app' + page)
+    // check for 200 status and if requests come in within 200ms
+    check(res, {
+      'status is 200': (r) => r.status === 200,
+      'duration was <=200ms': (r) => r.timings.duration <= 200,
+    })
+    sleep(1)
+  }
 }
