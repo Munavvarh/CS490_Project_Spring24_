a75ea0dfa58802ddda003f4bb728a4ce6077e307
patty-jean <pattydzw@msn.com>
2024-04-22 02:06:37 -0400

    Reformatting

--- web/src/pages/LoginPage/LoginPage.jsx
+++ web/src/pages/LoginPage/LoginPage.jsx
@@ -1,6 +1,5 @@
 import { useRef } from 'react'
 import { useEffect } from 'react'
-import MainLayout from 'src/layouts/MainLayout/MainLayout';
 
 import {
   Form,
@@ -15,7 +14,7 @@ import { Metadata } from '@redwoodjs/web'
 import { toast, Toaster } from '@redwoodjs/web/toast'
 
 import { useAuth } from 'src/auth'
-
+import MainLayout from 'src/layouts/MainLayout/MainLayout'
 
 const LoginPage = () => {
   const { isAuthenticated, logIn } = useAuth()
@@ -47,78 +46,77 @@ const LoginPage = () => {
   }
 
   return (
-
     <>
-    <MainLayout>
-      <Metadata title="Login" />
+      <MainLayout>
+        <Metadata title="Login" />
 
-      <main className="rw-main mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
-        <img
-          src="/logo.png"
-          alt="Syntax Switch Logo"
-          className="logo-image mr-2 h-8 w-8"
-        />
-        <Toaster toastOptions={{ className: 'rw-toast', duration: 6000 }} />
-        <div className="rw-scaffold rw-login-container shadow dark:border w-full rounded-lg bg-white sm:max-w-md md:mt-0 xl:p-0">
-          <div className="space-y-4 sm:p-8 md:space-y-6">
-            <div className="rw-segment">
-              <header className="rw-segment-header">
-                <h2 className="rw-heading rw-heading-secondary mb-6 text-xl font-bold leading-tight tracking-tight md:text-2xl">
-                  Sign in to your account!
-                </h2>
-              </header>
+        <main className="rw-main mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
+          <img
+            src="/logo-bg.png"
+            alt="Syntax Switch Logo"
+            className="logo-image"
+          />
+          <Toaster toastOptions={{ className: 'rw-toast', duration: 6000 }} />
+          <div className="rw-scaffold rw-login-container shadow dark:border w-full rounded-lg bg-white sm:max-w-md md:mt-0 xl:p-0">
+            <div className="space-y-4 sm:p-8 md:space-y-6">
+              <div className="rw-segment">
+                <header className="rw-segment-header">
+                  <h2 className="rw-heading rw-heading-secondary mb-6 text-xl font-bold leading-tight tracking-tight md:text-2xl">
+                    Sign in to your account!
+                  </h2>
+                </header>
 
-              <div className="rw-segment-main">
-                <div className="rw-form-wrapper">
-                  <Form
-                    onSubmit={onSubmit}
-                    className="rw-form-wrapper space-y-4 md:space-y-6"
-                  >
-                    <Label
-                      name="username"
-                      className="rw-label mb-2 block text-sm font-medium text-gray-900"
-                      errorClassName="rw-label rw-label-error"
+                <div className="rw-segment-main">
+                  <div className="rw-form-wrapper">
+                    <Form
+                      onSubmit={onSubmit}
+                      className="rw-form-wrapper space-y-4 md:space-y-6"
                     >
-                      Username
-                    </Label>
-                    <TextField
-                      name="username"
-                      className="rw-input bg-gray-50 border rounded focus:ring-primary-600 focus:border-primary-600 p-2.5 block w-full border-gray-300 text-gray-900 sm:text-sm"
-                      errorClassName="rw-input rw-input-error"
-                      placeholder="BMT"
-                      ref={usernameRef}
-                      validation={{
-                        required: {
-                          value: true,
-                          message: 'Username is required',
-                        },
-                      }}
-                    />
+                      <Label
+                        name="username"
+                        className="rw-label mb-2 block text-sm font-medium text-gray-900"
+                        errorClassName="rw-label rw-label-error"
+                      >
+                        Username
+                      </Label>
+                      <TextField
+                        name="username"
+                        className="rw-input bg-gray-50 border rounded focus:ring-primary-600 focus:border-primary-600 p-2.5 block w-full border-gray-300 text-gray-900 sm:text-sm"
+                        errorClassName="rw-input rw-input-error"
+                        placeholder="BMT"
+                        ref={usernameRef}
+                        validation={{
+                          required: {
+                            value: true,
+                            message: 'Username is required',
+                          },
+                        }}
+                      />
 
-                    <FieldError name="username" className="rw-field-error" />
+                      <FieldError name="username" className="rw-field-error" />
 
-                    <Label
-                      name="password"
-                      className="rw-label mb-2 block text-sm font-medium text-gray-900 "
-                      errorClassName="rw-label rw-label-error"
-                    >
-                      Password
-                    </Label>
-                    <PasswordField
-                      name="password"
-                      className="rw-input bg-gray-50 border rounded focus:ring-primary-600 focus:border-primary-600 p-2.5 block w-full border-gray-300 text-gray-900 sm:text-sm"
-                      errorClassName="rw-input rw-input-error"
-                      placeholder="**********"
-                      autoComplete="current-password"
-                      validation={{
-                        required: {
-                          value: true,
-                          message: 'Password is required',
-                        },
-                      }}
-                    />
+                      <Label
+                        name="password"
+                        className="rw-label mb-2 block text-sm font-medium text-gray-900 "
+                        errorClassName="rw-label rw-label-error"
+                      >
+                        Password
+                      </Label>
+                      <PasswordField
+                        name="password"
+                        className="rw-input bg-gray-50 border rounded focus:ring-primary-600 focus:border-primary-600 p-2.5 block w-full border-gray-300 text-gray-900 sm:text-sm"
+                        errorClassName="rw-input rw-input-error"
+                        placeholder="**********"
+                        autoComplete="current-password"
+                        validation={{
+                          required: {
+                            value: true,
+                            message: 'Password is required',
+                          },
+                        }}
+                      />
 
-                    {/* <div className="flex items-center justify-between">
+                      {/* <div className="flex items-center justify-between">
                       <div className="flex items-start">
                         <div className="flex items-center h-5">
                           <input id="remember" aria-describedby='remember' type="text" />
@@ -126,43 +124,42 @@ const LoginPage = () => {
                       </div>
                     </div> */}
 
-                    <div className="rw-forgot-link">
-                      <Link
-                        to={routes.forgotPassword()}
-                        className="rw-forgot-link text-primary-600 text-sm font-medium text-blue-500 hover:underline"
-                      >
-                        Forgot Password?
-                      </Link>
-                    </div>
+                      <div className="rw-forgot-link">
+                        <Link
+                          to={routes.forgotPassword()}
+                          className="rw-forgot-link text-primary-600 text-sm font-medium text-blue-500 hover:underline"
+                        >
+                          Forgot Password?
+                        </Link>
+                      </div>
 
-                    <FieldError name="password" className="rw-field-error" />
+                      <FieldError name="password" className="rw-field-error" />
 
-                    <div className="rw-button-group">
-                      <Submit className="rw-button rw-button-blue hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 py-2.5 w-full rounded-sm bg-blue-500 px-5 text-center text-sm font-medium text-white">
-                        Login
-                      </Submit>
-                    </div>
-                  </Form>
+                      <div className="rw-button-group">
+                        <Submit className="rw-button rw-button-blue hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 py-2.5 w-full rounded-sm bg-blue-500 px-5 text-center text-sm font-medium text-white">
+                          Login
+                        </Submit>
+                      </div>
+                    </Form>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="rw-login-link">
-              <span className="text-md font-light text-gray-500">
-                Don&apos;t have an account?
-              </span>{' '}
-              <Link
-                to={routes.signup()}
-                className="rw-link font-medium text-blue-500 hover:underline"
-              >
-                Sign up!
-              </Link>
+              <div className="rw-login-link">
+                <span className="text-md font-light text-gray-500">
+                  Don&apos;t have an account?
+                </span>{' '}
+                <Link
+                  to={routes.signup()}
+                  className="rw-link font-medium text-blue-500 hover:underline"
+                >
+                  Sign up!
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
-      </main>
+        </main>
       </MainLayout>
     </>
-   
   )
 }
 
